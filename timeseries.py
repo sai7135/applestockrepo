530a617e8f9a95235623e9b9a9a6264432deafbb
sai7135 <46988188+sai7135@users.noreply.github.com>
2023-04-09 13:09:21 +0530

    Update timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pickle
-emamodel=pickle.load(open('applestockrepo/main/model.pkl','rb'))
+emamodel=pickle.load(open('./model.pkl','rb'))
 startDate=st.date_input("Start Date")
 endDate=st.date_input("End Date")
 if st.button("predict"):
